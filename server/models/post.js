caff6818d508451f1c640853a6d52bfb8065533d
dukemetzo <metzod1@newpaltz.edu>
2025-06-16 21:02:31 -0400

    Completed Assignment 2

---
+++ server/models/post.js
@@ -0,0 +1,51 @@
+// 1. import mongoose
+const mongoose = require("mongoose");
+
+// 2. create schema for entity
+const postSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    postContent: { type: String, required: true },
+    postLikes: { type: Number, required: true, default: 0 },
+})
+
+// 3. create model of schema
+const Post = mongoose.model("Post", postSchema);
+
+// 4. create CRUD functions on model
+// CREATE a post
+async function createPost(userId, postContent) {
+    const newPost = await Post.create({
+        userId: userId,
+        postContent: postContent
+    });
+
+    return newPost;
+}
+
+// READ a post
+async function readPost(postId) {
+    const post = await Post.findById({"_id": postId});
+    if(!post) throw Error("Post not found");
+
+    return post;
+}
+
+// UPDATE
+async function likePost(postId) {
+    const post = await Post.findById({"_id": postId});
+    ++post.postLikes;
+    await post.save();
+    if(!post) throw Error("Post not found");
+
+    return post;
+}
+
+// DELETE
+async function deletePost(postId) {
+    await Post.findByIdAndDelete({"_id": postId});
+}
+
+// 5. export all functions we want to access in route files
+module.exports = {  
+    createPost, readPost, likePost, deletePost
+};
---
+++ server/routes/post.js
@@ -0,0 +1,45 @@
+// 1. import any needed libraries
+const express = require("express");
+const Post = require("../models/post"); //accesses functions in post model file
+const router = express.Router();
+
+// 2. create all routes to access database
+router
+    .post('/create', async (req, res) => {
+        try {
+            const post = await Post.createPost(req.body.userId, req.body.postContent);
+            res.send({...post});
+        } catch(error) {
+            res.status(401).send({ message: error.message });
+        }
+    })
+
+    .get('/read', async (req, res) => {
+        try {
+            const post = await Post.readPost(req.body.postId);
+            res.send({...post});
+        } catch(error) {
+            res.status(401).send({ message: error.message });
+        }
+    })
+
+    .put('/like', async (req, res) => {
+        try {
+            const post = await Post.likePost(req.body.postId);
+            res.send({ success: "Liked Post" });
+        } catch(error) {
+            res.status(401).send({ message: error.message });
+        }
+    })
+
+    .delete('/delete', async (req, res) => {
+        try {
+            await Post.deletePost(req.body.postId);
+            res.send({ success: "Post deleted" });
+        } catch(error) {
+            res.status(401).send({ message: error.message });
+        }
+    })
+
+    // 3. export router for use in index.js
+    module.exports = router;
